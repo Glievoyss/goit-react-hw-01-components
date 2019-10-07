a9dc359893f54d8caf6d0ba3950b43adefaa4a6a
Glievoyss <glievoy.s@gmail.com>
2019-10-07 11:19:26 +0300

    Updates

---
+++ precache-manifest.82d95e5031557cd76a73485129a23db7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398e8194e016c65509fdd5ddd8d60fdb",
+    "url": "Glievoyss.github.io/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f4d30865b87d39a0e5ba",
+    "url": "Glievoyss.github.io/goit-react-hw-01-components/static/css/main.6406b6c6.chunk.css"
+  },
+  {
+    "revision": "955f1f3b9b72a5995a63",
+    "url": "Glievoyss.github.io/goit-react-hw-01-components/static/js/2.eda023aa.chunk.js"
+  },
+  {
+    "revision": "f4d30865b87d39a0e5ba",
+    "url": "Glievoyss.github.io/goit-react-hw-01-components/static/js/main.2628684e.chunk.js"
+  },
+  {
+    "revision": "2bca6c57522853ea4bf0",
+    "url": "Glievoyss.github.io/goit-react-hw-01-components/static/js/runtime-main.ea8f2a5c.js"
+  }
+]);
